2d9ae3c560e17b6d188976282c2925315a908125
NoctisBeta <noctisbeta@gmail.com>
2025-06-01 18:25:35 +0200

    trash viewing

--- client/lib/board/areas/trash_area.dart
+++ client/lib/board/areas/trash_area.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:client/card_views/card_highlight_controller.dart';
 import 'package:client/card_views/character_card_view.dart';
 import 'package:client/constants.dart';
@@ -16,48 +18,107 @@ class TrashArea extends StatelessWidget {
     final double kCardWidth = kCardHeight * kCardAspectRatio;
     final List<DeckCard> cards = context.watch<Player>().trashCards;
 
-    return Stack(
-      children: [
-        Container(
-          width: kCardWidth,
-          height: kCardHeight,
-          decoration: BoxDecoration(
-            border: Border.all(),
-            color: Colors.grey[300],
-          ),
-          child: FittedBox(
-            child: Text(
-              'Trash',
-              style: TextStyle(color: Colors.grey[400]),
-              textAlign: TextAlign.center,
+    return GestureDetector(
+      onTap: () {
+        if (cards.isEmpty) {
+          return;
+        }
+
+        unawaited(
+          showDialog(
+            context: context,
+            barrierColor: Colors.transparent,
+            builder: (BuildContext dialogContext) => AlertDialog(
+              title: const Text('Trash'),
+              content: SizedBox(
+                width: 200,
+                height: 300,
+                child: GridView.builder(
+                  itemCount: cards.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3, // Adjust as needed
+                    childAspectRatio: kCardAspectRatio,
+                    crossAxisSpacing: kPadding,
+                    mainAxisSpacing: kPadding,
+                  ),
+                  itemBuilder: (BuildContext context, int index) {
+                    final DeckCard card = cards[index];
+                    return MouseRegion(
+                      onEnter: (_) => context
+                          .read<CardHighlightController>()
+                          .highlightCard(card),
+                      onExit: (_) => context
+                          .read<CardHighlightController>()
+                          .clearHighlight(),
+                      child: switch (card) {
+                        CharacterCard() => SizedBox(
+                          width: kCardWidth,
+                          height: kCardHeight,
+                          child: CharacterCardView(
+                            card: card,
+                          ),
+                        ),
+                        _ => const SizedBox.shrink(),
+                      },
+                    );
+                  },
+                ),
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text('Close'),
+                  onPressed: () {
+                    Navigator.of(dialogContext).pop();
+                  },
+                ),
+              ],
             ),
           ),
-        ),
-        ...cards.indexed.map(
-          (tuple) => Positioned(
-            left: 0,
-            top: 0,
-            child: SizedBox(
-              width: kCardWidth,
-              height: kCardHeight,
-              child: MouseRegion(
-                onEnter: (_) => context
-                    .read<CardHighlightController>()
-                    .highlightCard(tuple.$2),
-                onExit: (_) =>
-                    context.read<CardHighlightController>().clearHighlight(),
-                child: switch (tuple.$2) {
-                  CharacterCard() => CharacterCardView(
-                    card: tuple.$2 as CharacterCard,
-                  ),
-
-                  _ => const SizedBox.shrink(),
-                },
+        );
+      },
+      child: Stack(
+        children: [
+          Container(
+            width: kCardWidth,
+            height: kCardHeight,
+            decoration: BoxDecoration(
+              border: Border.all(),
+              color: Colors.grey[300],
+            ),
+            child: FittedBox(
+              child: Text(
+                'Trash',
+                style: TextStyle(color: Colors.grey[400]),
+                textAlign: TextAlign.center,
               ),
             ),
           ),
-        ),
-      ],
+          ...cards.indexed.map(
+            (tuple) => Positioned(
+              left: 0,
+              top: 0,
+              child: SizedBox(
+                width: kCardWidth,
+                height: kCardHeight,
+                child: MouseRegion(
+                  onEnter: (_) => context
+                      .read<CardHighlightController>()
+                      .highlightCard(tuple.$2),
+                  onExit: (_) =>
+                      context.read<CardHighlightController>().clearHighlight(),
+                  child: switch (tuple.$2) {
+                    CharacterCard() => CharacterCardView(
+                      card: tuple.$2 as CharacterCard,
+                    ),
+
+                    _ => const SizedBox.shrink(),
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
